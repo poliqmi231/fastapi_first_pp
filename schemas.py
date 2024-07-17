b93251db8f1eb6ae9db1abd58da3f23ca05f4e19
poliqmi231 <NE0Nbr0@yandex.ru>
2024-07-17 06:07:28 +0300

    fix schemas bug (add task)

--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import Optional
 
 class STaskAdd(BaseModel):
@@ -9,6 +9,7 @@ class STaskAdd(BaseModel):
 class STask(STaskAdd):
     id: int
 
+    model_config = ConfigDict(from_attributes=True)
 
 class STaskId(BaseModel):
     ok: bool = True
